8692badc9020309f90ed7e12cd75283533793a77
root <root@ip-172-31-82-203.ec2.internal>
2023-05-08 19:27:37 +0000

    v3

--- website_deploy.sh
+++ website_deploy.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
-
 cd ./packer/
-
+packer init .
+if [[ $? != 0 ]]
+  then
+    echo "Something wrong while packer init"
+    exit 1
+fi
 packer build aws-ubuntu.pkr.hcl
 if [[ $? != 0 ]]
   then
     echo "Something wrong while executing packer build"
     exit 1
 fi
-
 cd ../terraform/
-
 terraform init
 if [[ $? != 0 ]]; then
-	echo "Something wrong while executing terraform init"
-	exit 1
+        echo "Something wrong while executing terraform init"
+        exit 1
 fi
-
 terraform plan
 if [[ $? != 0 ]]; then
-	echo "Something wrong while executing terraform plan"
-	exit 1
+        echo "Something wrong while executing terraform plan"
+        exit 1
 fi
-
 terraform apply -auto-approve
 if [[ $? != 0 ]]; then
-	echo "Something wrong while executing terraform apply"
-	exit 1
+        echo "Something wrong while executing terraform apply"
+        exit 1
 fi
-
-URL=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=Website' --query 'Reservations[].Instances[].PublicIpAddress' --output text)
+URL=$(aws ec2 describe-instances --filters Name=tag:Name,Values=Website --region us-east-1 --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 if [[ $? != 0 ]]
   then
     echo "Something wrong while getting website IP"
     exit 1
 fi
-
 echo "website ip is ${URL}"
